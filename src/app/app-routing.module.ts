06ab3a2a2e2e535be3fc74e2209accaa3b0506ae
Matias Kupfer <matiascfgm@gmail.com>
2019-11-15 09:48:53 +0100

    added navbar, angular material and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
 import { NewProductComponent } from './new-product/new-product.component';
 import { BuyProductComponent } from './buy-product/buy-product.component';
 
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'new-product', component: NewProductComponent },
   { path: 'buy-product', component: BuyProductComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,25 +10,31 @@ import { BuyProductComponent } from './buy-product/buy-product.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MaterialModule} from './style/material.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule } from '@angular/material/radio';
 import {TextFieldModule} from '@angular/cdk/text-field';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewProductComponent,
     BuyProductComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
+    MaterialModule,
     MatToolbarModule,
     MatInputModule,
     MatButtonModule,
